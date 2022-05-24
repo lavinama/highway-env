148810fa6893ee826c57488433e93ee9f3e93b84
tibigg <tg4018@ic.ac.uk>
2022-05-24 19:18:51 +0100

    Redefined gridlock to use intermediate nodes, for better path planning

--- highway_env/envs/gridlock_env.py
+++ highway_env/envs/gridlock_env.py
@@ -25,7 +25,7 @@ class GridlockEnv(AbstractEnv):
     NUM_ROADS = 4
     ROAD_LENGTH = 30  # [m]
     DISTANCE_BETWEEN_VEHICLES = 2
-    DISTANCE_BETWEEN_ROADS = 2.5
+    DISTANCE_BETWEEN_ROADS = 10
 
     @classmethod
     def default_config(cls) -> dict:
@@ -59,13 +59,13 @@ class GridlockEnv(AbstractEnv):
                 }
             },
             "duration": 13,  # [s]
-            "destination": "o0",
+            "destination": "o1",
             "controlled_vehicles": 8,
             "initial_vehicle_count": 0,
             "spawn_probability": 0,
             "screen_width": 600,
             "screen_height": 600,
-            "centering_position": [0.5, 0.6],
+            "centering_position": [0.5, 0.5],
             "scaling": 5.5 * 1.3,
             "collision_reward": -100,
             "high_speed_reward": 1,
@@ -78,7 +78,7 @@ class GridlockEnv(AbstractEnv):
 
     def __init__(self, config: dict = None) -> None:
         super().__init__(config)
-        self.viewer = StaticEnvViewer(self, offset=np.array([self.DISTANCE_BETWEEN_ROADS, self.DISTANCE_BETWEEN_ROADS + 5]))
+        self.viewer = StaticEnvViewer(self, offset=np.array([0, 0]))
 
     def _reward(self, action: int) -> float:
         # Cooperative multi-agent reward
@@ -129,58 +129,63 @@ class GridlockEnv(AbstractEnv):
         Make an 4-way intersection.
 
         The horizontal road has the right of way. More precisely, the levels of priority are:
-            - 3 for horizontal straight lanes and right-turns
-            - 1 for vertical straight lanes and right-turns
-            - 2 for horizontal left-turns
-            - 0 for vertical left-turns
+            - 2 for right-turns
+            - 1 for straight lanes
+            - 0 for left-turns
 
         The code for nodes in the road network is:
-        (o:outer | i:inner + [r:right, l:left]) + (0:south | 1:west | 2:north | 3:east)
+        (o:outer | (i:inner | c:central) + [r:right, l:left]) + (0:south | 1:west | 2:north | 3:east)
 
         :return: the intersection road
         """
         lane_width = AbstractLane.DEFAULT_WIDTH
-        right_turn_radius = lane_width + 5 # [m}
-        left_turn_radius = right_turn_radius - 5 # [m}
-        outer_distance = right_turn_radius + lane_width / 2 + self.DISTANCE_BETWEEN_ROADS
+        turn_radius = lane_width  # [m}
+        outer_distance = turn_radius + lane_width / 2 + self.DISTANCE_BETWEEN_ROADS / 2
+        inner_distance = -turn_radius + lane_width / 2 + self.DISTANCE_BETWEEN_ROADS / 2
         access_length = self.ROAD_LENGTH  # [m]
-        lateral_offset = lane_width / 2 + self.DISTANCE_BETWEEN_ROADS
+        lateral_offset = lane_width / 2 + self.DISTANCE_BETWEEN_ROADS / 2
+        max_priority = 2
 
         net = RoadNetwork()
         n, c, s = LineType.NONE, LineType.CONTINUOUS, LineType.STRIPED
         for corner in range(self.NUM_ROADS):
             angle = np.radians(90 * corner)
-            is_horizontal = corner % 2
-            priority = 3 if is_horizontal else 1
             rotation = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
             # Incoming
             start = rotation @ np.array([lateral_offset, access_length + outer_distance])
             end = rotation @ np.array([lateral_offset, outer_distance])
             net.add_lane("o" + str(corner), "ir" + str(corner),
-                         StraightLane(start, end, line_types=[c, c], priority=priority, speed_limit=10))
+                         StraightLane(start, end, line_types=[c, c], priority=max_priority, speed_limit=10))
             # Right turn
             r_center = rotation @ (np.array([outer_distance, outer_distance]))
             net.add_lane("ir" + str(corner), "il" + str((corner - 1) % self.NUM_ROADS),
-                         CircularLane(r_center, right_turn_radius, angle + np.radians(180), angle + np.radians(270),
-                                      line_types=[n, c], priority=priority, speed_limit=10))
+                         CircularLane(r_center, turn_radius, angle + np.radians(180), angle + np.radians(270),
+                                      line_types=[n, c], priority=max_priority, speed_limit=10))
             # Left turn
-            l_center = rotation @ (np.array([-left_turn_radius + lane_width / 2 + self.DISTANCE_BETWEEN_ROADS, left_turn_radius - lane_width / 2 - self.DISTANCE_BETWEEN_ROADS]))
-            net.add_lane("ir" + str(corner), "il" + str((corner + 1) % self.NUM_ROADS),
-                         CircularLane(l_center, left_turn_radius, angle + np.radians(0), angle + np.radians(-90),
-                                      clockwise=False, line_types=[s, n], priority=priority - 1, speed_limit=10))
+            l_center = rotation @ (np.array([inner_distance, -inner_distance]))
+            net.add_lane("cr" + str((corner - 1) % self.NUM_ROADS), "cl" + str((corner - 2) % self.NUM_ROADS),
+                         CircularLane(l_center, turn_radius, angle + np.radians(0), angle + np.radians(-90),
+                                      clockwise=False, line_types=[c, s], priority=max_priority - 2, speed_limit=10))
+
             # Straight
             start = rotation @ np.array([lateral_offset, outer_distance])
+            mid1 = rotation @ np.array([lateral_offset, inner_distance])
+            mid2 = rotation @ np.array([lateral_offset, -inner_distance])
             end = rotation @ np.array([lateral_offset, -outer_distance])
-            net.add_lane("ir" + str(corner), "il" + str((corner + 2) % self.NUM_ROADS),
-                         StraightLane(start, end, line_types=[s, n], priority=priority, speed_limit=10))
+            net.add_lane("ir" + str(corner), "cl" + str((corner - 1) % self.NUM_ROADS),
+                         StraightLane(start, mid1, line_types=[n, s], priority=max_priority - 1, speed_limit=10))
+            net.add_lane("cl" + str((corner - 1) % self.NUM_ROADS), "cr" + str((corner - 1) % self.NUM_ROADS),
+                         StraightLane(mid1, mid2, line_types=[c, c], priority=max_priority - 1, speed_limit=10))
+            net.add_lane("cr" + str((corner - 1) % self.NUM_ROADS), "il" + str((corner + 2) % self.NUM_ROADS),
+                         StraightLane(mid2, end, line_types=[n, s], priority=max_priority - 1, speed_limit=10))
             # Exit
             start = rotation @ np.flip([lateral_offset, access_length + outer_distance], axis=0)
             end = rotation @ np.flip([lateral_offset, outer_distance], axis=0)
             net.add_lane("il" + str((corner - 1) % self.NUM_ROADS), "o" + str((corner - 1) % self.NUM_ROADS),
-                         StraightLane(end, start, line_types=[c, c], priority=priority, speed_limit=10))
+                         StraightLane(end, start, line_types=[c, c], priority=max_priority, speed_limit=10))
 
-        road = RegulatedRoad(network=net, np_random=self.np_random, record_history=self.config["show_trajectories"])
-        self.road = road
+            road = RegulatedRoad(network=net, np_random=self.np_random, record_history=self.config["show_trajectories"])
+            self.road = road
 
     def _make_vehicles(self, n_vehicles: int = 10) -> None:
         """
@@ -214,10 +219,11 @@ class GridlockEnv(AbstractEnv):
                  0)
             )
             destination = self.config["destination"]\
-                          or "o" + str(self.np_random.randint(1, self.NUM_ROADS))
+                          or "o" + str((ego_id + self.np_random.randint(0, 2)) % self.NUM_ROADS)
             offsets[ego_id % self.NUM_ROADS] += self.np_random.rand(1)
-            ego_position = ego_lane.position(self.ROAD_LENGTH + 14 - offsets[ego_id % self.NUM_ROADS],
-                                             ((self.np_random.rand(1) * 2) - 1))
+            ego_position = ego_lane.position(
+                self.ROAD_LENGTH + self.DISTANCE_BETWEEN_ROADS + 2 - offsets[ego_id % self.NUM_ROADS],
+                ((self.np_random.rand(1) * 2) - 1))
             ego_heading = ego_lane.heading + \
                           ((self.np_random.rand(1) * 2) - 1)[0] * np.pi / 12
             ego_vehicle = self.action_type.vehicle_class(self.road, ego_position, ego_heading, 0)
@@ -277,7 +283,7 @@ class GridlockEnv(AbstractEnv):
         return float(self.vehicle.crashed)
 
 
-register(
-    id='gridlock-v0',
-    entry_point='highway_env.envs:GridlockEnv',
-)
+    register(
+        id='gridlock-v0',
+        entry_point='highway_env.envs:GridlockEnv',
+    )
