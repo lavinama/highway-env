31ed9473bae7b157407831beece33cf9841900b8
lavinama <mario.lavina00@gmail.com>
2022-08-13 18:21:56 +0200

    Update reg_env

--- misc/reg_env.py
+++ misc/reg_env.py
@@ -1,7 +1,7 @@
 from gym import envs
 from gym.envs.registration import register
 
-all_envs = envs.registry.all()
+all_envs = envs.registry.values
 env_ids = [env_spec.id for env_spec in all_envs]
 ids = sorted(env_ids)
 for id in ids:
