ea683c76669242dcea4653781e231ea3a1936959
lavinama <mario.lavina00@gmail.com>
2022-08-13 16:02:17 +0100

    update __init__ file

--- highway_env/envs/__init__.py
+++ highway_env/envs/__init__.py
@@ -1,4 +1,4 @@
-from highway_env.envs.highway_env_script import *
+# from highway_env.envs.highway_env_script import *
 from highway_env.envs.merge_env import *
 from highway_env.envs.parking_env import *
 from highway_env.envs.summon_env import *
